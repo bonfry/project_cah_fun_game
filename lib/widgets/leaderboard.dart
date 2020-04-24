29f108b1d8a4a2c1478ab150d241e4ce574b23fb
bonfry <bonfry98@live.it>
2020-04-24 15:05:53 +0200

    optimize leaderboard for a lot of players in the same session

--- lib/widgets/leaderboard.dart
+++ lib/widgets/leaderboard.dart
@@ -4,7 +4,7 @@ import 'package:flutter_icons/flutter_icons.dart';
 
 class Leaderboard extends StatelessWidget {
   final String blackKingPlayer;
-  final int wcRequirement;
+  final String currentPlayerApplication;
   final Map<String, PlayerDetail> playersMap;
 
   static const Icon _blackKingIcon = Icon(MaterialCommunityIcons.chess_king);
@@ -16,12 +16,22 @@ class Leaderboard extends StatelessWidget {
   const Leaderboard(
       {Key key,
       @required this.playersMap,
-      @required this.wcRequirement,
+      @required this.currentPlayerApplication,
       @required this.blackKingPlayer})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    var playersOrderByScore = playersMap.keys.toList()
+      ..sort((username1,username2){
+        var userPoints1 = playersMap[username1].points;
+        var userPoints2 = playersMap[username2].points;
+
+        return userPoints1.compareTo(userPoints2)*-1;
+      });
+    
+
     return Card(
       child: Padding(
         padding: EdgeInsets.all(10),
@@ -32,42 +42,49 @@ class Leaderboard extends StatelessWidget {
               'Leaderboard',
               style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
             ),
-            ...playersMap.keys.map((username) {
-              double iconOpacity =
-                  (playersMap[username].hasSent || blackKingPlayer == username)
-                      ? 1
-                      : 0;
-
-              var iconToShow =
-                  blackKingPlayer == username ? _blackKingIcon : _hasSentIcon;
-
-              return Container(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Row(
-                    children: <Widget>[
-                      Opacity(
-                        opacity: iconOpacity,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 10),
-                          child: iconToShow,
-                        ),
-                      ),
-                      Text(username),
-                      Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            playersMap[username].points.toString(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 14,
-                                color: Colors.blue[700]),
-                          ))
-                    ],
-                  ));
-            }),
+            ...playersOrderByScore.sublist(0,5).map(createPlayerLabel),
+            Divider(
+              color: Colors.grey,
+              thickness: 5,
+            ),
+            createPlayerLabel(currentPlayerApplication)
           ],
         ),
       ),
     );
+  }
+
+  Widget createPlayerLabel(String username){
+    double iconOpacity =
+    (playersMap[username].hasSent || blackKingPlayer == username)
+        ? 1
+        : 0;
+
+    var iconToShow =
+    blackKingPlayer == username ? _blackKingIcon : _hasSentIcon;
+
+    return Container(
+        padding: EdgeInsets.only(top: 10),
+        child: Row(
+          children: <Widget>[
+            Opacity(
+              opacity: iconOpacity,
+              child: Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: iconToShow,
+              ),
+            ),
+            Text(username),
+            Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text(
+                  playersMap[username].points.toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: Colors.blue[700]),
+                ))
+          ],
+        ));
   }
 }

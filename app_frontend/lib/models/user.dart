6fe02b42286649e0e8107be37ee4868b952edf3d
bonfry <bonfry98@live.it>
2020-08-14 09:03:45 +0000

    refactor user for use it as singleton

--- app_frontend/lib/models/user.dart
+++ app_frontend/lib/models/user.dart
@@ -6,52 +6,58 @@ import 'package:projectcahfungame/session_data.dart';
 const String USER_SHARED_PREFERENCES_LABEL = 'user_config';
 
 class User {
+  static User _loggedUser;
+
   final String username;
   final String token;
 
-  User({@required this.username, this.token});
+  User._internal({@required this.username, this.token});
 
-  Map<String, String> toMap() {
+  User.fromJson(Map<String, dynamic> userDataMap) : 
+    username =  userDataMap['username'] as String,
+    token =  userDataMap['user_token'] as String;
+
+  factory User(){
+    return _loggedUser;
+  }
+
+  Map<String, dynamic> toJson() {
     return {'username': username, 'user_token': token};
   }
 
-  static User parseUser(Map<String, String> userDataMap) {
-    return User(
-        username: userDataMap['username'], token: userDataMap['user_token']);
-  }
+
 
   static Future<User> getInstance() async {
     var sharedPrefs = await SessionData.sharedPreferences;
-    User user;
+    User user = _loggedUser;
 
-    if (!sharedPrefs.containsKey(USER_SHARED_PREFERENCES_LABEL)) {
+    if (user != null || !sharedPrefs.containsKey(USER_SHARED_PREFERENCES_LABEL)) {
       return user;
     }
 
     var userConfigText = sharedPrefs.getString(USER_SHARED_PREFERENCES_LABEL);
-    var jsonMap = JsonDecoder().convert(userConfigText);
+    var jsonMap = jsonDecode(userConfigText);
 
     if (jsonMap.containsKey('username') && jsonMap.containsKey('user_token')) {
-      user = User.parseUser(jsonMap);
+      user = User.fromJson(jsonMap);
+      _loggedUser = user;
     }
 
     return user;
   }
 
-  static User login(String username, String token, {bool saveLogin = false}) {
-    User user;
-
+  static User login(String username, String token, {bool saveLogin = true}) {
     if (username != null && token != null) {
-      user = User(username: username, token: token);
+      _loggedUser = User._internal(username: username, token: token);
 
       if (saveLogin) {
-        var jsonText = JsonEncoder().convert(user.toMap());
+        var jsonText =jsonEncode(_loggedUser);
 
         SessionData.sharedPreferences.then((sharedPrefs) =>
             sharedPrefs.setString(USER_SHARED_PREFERENCES_LABEL, jsonText));
       }
     }
 
-    return user;
+    return _loggedUser;
   }
 }
